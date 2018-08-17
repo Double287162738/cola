b9051e9b003448b340809e8f37cd0712df4ccdfa
Double287162738 <287162738@qq.com>
2018-08-17 16:46:21 +0800

    Actual Sales Creating commit

---
+++ src/main/java/com/coca/util/ColaConstant.java
@@ -0,0 +1,22 @@
+package com.coca.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Administrator
+ */
+public final class ColaConstant {
+
+
+    //返回结果成功
+    public static final String RESULT_SUCCESS = "SUCCESS";
+    //返回结果失败
+    public static final String RESULT_FAIL = "FAIL";
+    //返回结果未登录
+    public static final String NOT_LOGIN = "NOTLOGIN";
+
+    public static final List<String> MONTH = Arrays.asList("JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DECE");
+
+}
